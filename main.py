72c8465a2d32caa83a753130c19aec46df86ef28
jimlin2004 <jim.lin2004@gmail.com>
2025-04-11 22:53:52 +0800

    修改 - jimlin

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ if (__name__ == "__main__"):
                 for _ in range(tasks[task_i].evaluate_steps):
                     train_actor(actor, actor_optimizer, critic, replay_buffer)
 
-        indv_ranking: list[np.ndarray] = [None for _ in range(tasks)]
+        indv_ranking: list[np.ndarray] = [None for _ in range(len(tasks))]
         for task_i in range(len(tasks)):
 
             if (tasks[task_i].is_reach_steps_limit()):
